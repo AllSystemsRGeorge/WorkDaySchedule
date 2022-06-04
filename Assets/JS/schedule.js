6be52b80dda57026759d6213c1f6824c67a2a1d3
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-06-03 17:54:01 -0700

    Background function working!

--- Assets/JS/schedule.js
+++ Assets/JS/schedule.js
@@ -2,56 +2,36 @@
 var currentDate = moment().format('dddd, MMMM Do');
 $('#date').text(currentDate);
 
-console.log(date);
-
 // The function for the current time
-var currentHour = moment().format('H');
-
-console.log(currentHour);
-
-// var backgroundColor = $('li').backgroundColor
-
-listItems = $("#selectable").find("li").each(function(){
-  var product = $(this);
-  console.log(product);
-});
+var currentHour = moment().format('h');
+// $('.time').text(currentHour);
 
 
-// function changeBackground(color) {
-//   var listItems = $("#selectable li");
-//   listItems.each(function (idx, li) {
-//     var product = $(li);
-//     console.log(product.style.backgroundColor);
-//   });
-// };
 
-// const rows = document.getElementById("#selectable");
-// let currentHour = parseInt(moment().format('H'));
+var listItems = $("#selectable").find("li");
+var i, len, item;
 
-// Array.from(rows).forEach(row => {
-//   console.log(row);
-//   let
-//     rowIdString = row.id,
-//     rowHour;
-//   if (rowIdString) {
-//     rowHour = parseInt(rowIdString);
-//   }
-//   if (rowHour) {
-//     // Compares row id to current hour and sets color accordingly
-//     if (currentHour === rowHour) {
-//       setColor(row, "red");
-//     } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-//       setColor(row, "green");
-//     } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-//       setColor(row, "lightgrey");
-//     } else {
-//       setColor(row, "white");
-//     }
-//   }
-// });
-
-// function setColor(element, color) {
-//   element.style.backgroundColor = color;
-// }
-
+for (i = 0, len = listItems.length; i < len; i++) {
+  item = $(listItems[i]);
+  console.log(i);
+  console.log(item);
+  var modifier = 0
+  let currentTime = moment(parseInt(currentHour)).isBetween(i + modifier, i + 2 + modifier);
+  var past = moment(parseInt(currentHour)).isBefore(i + modifier);
+  var future = moment(parseInt(currentHour)).isAfter(i + modifier);
+  console.log(currentTime);
+  console.log(currentHour);
+  if (currentHour) {
+    // Compares id to current hour and sets color accordingly
+    if (currentTime) {
+      item.css("background-color", "red");
+    }
+    if (future) {
+      item.css("background-color", "green");
+    }
+    if (past) {
+      item.css("background-color", "grey");
+    }
+  }
+};
 
